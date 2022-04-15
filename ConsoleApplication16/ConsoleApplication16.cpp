1e19f99107b277c7588bde20fde2547515f23510
User <User@WIN-A4TOTEJDR9Q>
2022-04-15 20:42:14 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication16/ConsoleApplication16.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+void factor(int n)
+{
+    int k;
+    cout << "Input n = ";
+    cin >> n;
+    for (n = 1; n; n++)
+    {
+        (n-1)*n;
+    }
+    return factor();
+}
+    

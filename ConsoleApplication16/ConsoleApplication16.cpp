ae92354562b422f31258f812788ea79c5b401b42
UserAndrey <36071761+UserAndrey@users.noreply.github.com>
2023-01-31 11:48:32 +0300

    Update ConsoleApplication16.cpp

--- ConsoleApplication16/ConsoleApplication16.cpp
+++ ConsoleApplication16/ConsoleApplication16.cpp
@@ -1,15 +1,19 @@
-﻿#include <iostream>
-using namespace std;
-
-void factor(int n)
-{
-    int k;
-    cout << "Input n = ";
-    cin >> n;
-    for (n = 1; n; n++)
-    {
-        (n-1)*n;
-    }
-    return factor();
-}
-    
+#include <stdio.h>
+#include <conio.h>
+ unsigned long long int factor(int n) {
+ if (n <= 1) return 1;
+ return n*factor(n-1);
+                            }
+int main() 
+ {int n=1,k;
+ printf("Input k(!) = ");
+ scanf("%i",&k);
+ while (n<=k)
+ { 
+   printf("%llu\n",factor(n));
+ n++;
+ }
+ getch();
+ return 0; 
+ }
+ 
